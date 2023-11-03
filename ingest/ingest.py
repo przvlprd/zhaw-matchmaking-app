dd3e0d6ad29310be3ea65a6e3052fb3ecbf6dc42
przvlprd <przvlprd@pm.me>
2023-11-04 00:40:30 +0100

    minor fixes

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -20,17 +20,14 @@ if (PINECONE_INDEX_NAME := os.environ.get("PINECONE_INDEX")) is None:
 if __name__ == "__main__":
     # Load
     data = loader.load()
-
     # Split
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,
                                                    chunk_overlap=0)
     all_splits = text_splitter.split_documents(data)
-
     # Add to vectorDB
     Pinecone.from_documents(
         documents=all_splits,
         embedding=OpenAIEmbeddings(show_progress_bar=True),
         index_name=PINECONE_INDEX_NAME
     )
-
     print("Done.")
