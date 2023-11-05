40a79243093e92d950af5dcc120dec1b43c016ad
przvlprd <przvlprd@pm.me>
2023-11-05 23:47:19 +0100

    added streaming endpoint

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, conint
 from enum import Enum
-from retrieve import get_relevant_results
+from pydantic import BaseModel, conint
+
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
 from chain import get_context
+from retrieve import get_relevant_results
 
 
 class SearchOptions(str, Enum):
@@ -32,9 +35,29 @@ def query(user_input_request: UserInputRequest):
     for name, url in zip(results[0], results[1]):
         message += f"\n{name}\n{url}\n"
         context = get_context(name, url, user_input)
-        message += context
+        message += context[1:] + "\n"
 
     return {'output': message}
+
+
+@app.post("/query-stream/")
+async def query_stream(user_input_request: UserInputRequest):
+    user_input = user_input_request.user_input
+    search = user_input_request.search
+    k = user_input_request.k
+
+    results = get_relevant_results(user_input, search, k)
+
+    def generate_response():
+        yield "Folgende Mitarbeiter könnten interessant für dich sein:\n"
+
+        for name, url in zip(results[0], results[1]):
+            new_message = f"\n{name}\n{url}\n"
+            context = get_context(name, url, user_input)
+            new_message += context[1:] + "\n"
+            yield new_message
+
+    return StreamingResponse(content=generate_response())
 
 
 if __name__ == "__main__":
